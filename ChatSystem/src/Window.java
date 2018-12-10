14bde792e52f270b118b288c25f3b798d5e9b204
KimiNako <katran@etud.insa-toulouse.fr>
2018-12-10 18:16:52 +0100

    Add View LaunSession

--- ChatSystem/src/Window.java
+++ ChatSystem/src/Window.java
@@ -1,16 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
-public class Window {
+public class Window implements ActionListener {
+	public String pseudo;
+	public JFrame f; 
 	
 	public Window() {
 		
-		JFrame f = new JFrame(); //Creating instance of JFrame
-		
-		// Add button "create Account"
-		JButton b = new JButton("Create account");
-		b.setBounds(130,100,300,40); // x y width height
-		
-		f.add(b);
+		 this.f = new JFrame(); //Creating instance of JFrame
 		
 		//Add filed for pseudo
 		JTextField fieldPseudo = new JTextField();
@@ -22,13 +21,40 @@ public class Window {
 		lblPseudo.setBounds(65, 31, 70, 14);
 		f.getContentPane().add(lblPseudo);
 		
+		this.pseudo = fieldPseudo.getText(); 
+		
+		// Add button "create Account"
+		JButton b = new JButton("Create account");
+		b.setBounds(130,100,300,40); // x y width height
+		b.addActionListener(this);
+		f.add(b);
+		
 		
 		f.setSize(500,500);//width x height  
 		f.setLayout(null);
 		f.setVisible(true);
 	}
-	
-	public void refreshWindow() {
+
+	public void launchConnection() {
+		Controller c = new Controller(this);
+		c.createAgent(this.pseudo);
 		
+		// Add button "Launch Session"
+		JButton b = new JButton("Launch Session");
+		b.setBounds(130,100,300,40); // x y width height
+		b.addActionListener(this);
+		f.add(b);
+		f.setLayout(null);
+		f.setContentPane(b);
+
+		f.repaint();
+
+		f.setVisible(true);
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		launchConnection();
+	}
+	
+
 }
