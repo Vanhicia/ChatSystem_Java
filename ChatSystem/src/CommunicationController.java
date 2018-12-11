391aa18d4b72012cbe8b214575e4770aa0f7f1c7
KimiNako <katran@etud.insa-toulouse.fr>
2018-12-11 18:17:05 +0100

    Try to do a port scanner

--- ChatSystem/src/CommunicationController.java
+++ ChatSystem/src/CommunicationController.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.List;
 
 public class CommunicationController {
@@ -17,4 +19,22 @@ public class CommunicationController {
 	}
 	
 	
+	//Check if a server is listening on a port number (that is between 1 and 1024)   
+	public void portScanner() {
+		int i=1;
+		boolean connect = false;
+		while(i<1024 && !connect) {
+			try {
+				Socket link = new Socket(InetAddress.getLocalHost(), i) ;
+				System.out.println("Server is listening on port "+i+" of localhost");
+				connect = true ;
+			} catch (UnknownHostException e) {
+				System.out.println("Server is not listening on port "+i+" of localhost");
+			} catch (IOException e) {
+				System.out.println("Server is not listening on port "+i+" of localhost");
+			}
+			i++;
+		}
+		this.port = i;
+	}
 }
